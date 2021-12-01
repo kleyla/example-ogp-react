4c27df77b787dfd7e27b17aff335448a4fdbb7ad
kleyla <kleyla01@live.com>
2021-12-01 08:58:44 -0400

    add example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <a
+        href={`https://api.whatsapp.com/send?text=Hola%0a${window.location.href}`}
+        data-action="share/whatsapp/share"
+        target="_blank"
+      >
+        Share
+      </a>
     </div>
   );
 }
